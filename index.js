55b55871ce0d052b2a99f74170ca513a81d3fe04
Jeff Lang <jeffplang@gmail.com>
2023-01-01 19:53:06 -0500

    demo of rpio button reading to send websocket event

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const WebSocketServer = require('ws');
+const rpio = require('rpio');
 
 const wss = new WebSocketServer.Server({ port: 8080 })
- 
+
+rpio.open(15, rpio.INPUT, rpio.PULL_UP);
+
 wss.on("connection", ws => {
+    function pollcb(pin)
+    {
+        /*
+         * Wait for a small period of time to avoid rapid changes which
+         * can't all be caught with the 1ms polling frequency.  If the
+         * pin is no longer down after the wait then ignore it.
+         */
+        rpio.msleep(20);
+
+        if (rpio.read(pin))
+                return;
+        ws.send("foobar!");
+        console.log('Button pressed on pin P%d', pin);
+    }
+
+  
+    rpio.poll(15, pollcb, rpio.POLL_LOW);
+
     ws.on("message", data => {
         console.log(`Client has sent us: ${data}`)
     });
@@ -15,4 +36,6 @@ wss.on("connection", ws => {
         console.log("Some Error occurred")
     }
 });
+
 console.log("The WebSocket server is running on port 8080");
+
